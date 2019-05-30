9f56f2f92d55caa11fcc9b8c436e96977892ceba
sb-bilal-dev <saidbilol18@gmail.com>
2019-05-30 16:46:53 +0500

    remove obsolete hot-update boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,7 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-import Root from './Root'
-import './index.css'
+import Root from "./Root";
+import "./index.css";
 
-const render = (Root: React.ComponentClass) => {
-  ReactDOM.render(
-    <Root />,
-    document.getElementById('root')
-  )
-}
-
-render(Root)
-if (module.hot) {
-  module.hot.accept('./Root', () => {
-    const NewRoot = require('./Root').default
-    render(NewRoot)
-  })
-}
+ReactDOM.render(<Root />, document.getElementById("root"));
